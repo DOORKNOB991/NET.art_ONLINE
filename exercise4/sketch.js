0854518a8caaa2bb96ccea3fb1c1619a7a5995e6
DOORKNOB991 <103093455+DOORKNOB991@users.noreply.github.com>
2023-10-16 22:48:35 -0400

    Add files via upload

---
+++ exercise4/sketch.js
@@ -0,0 +1,40 @@
+function setup() {
+  createCanvas(400, 400);
+}
+function draw() {
+  fill(91, 149, 82);
+  noStroke();
+  rect(0,0,1000,1000);
+  
+  fill(90, 172, 162);
+  noStroke();
+  rect(0,40,1000,100);
+  
+  fill(176, 191, 176);
+  noStroke();
+  rect(20,50,350,250);
+  
+  fill(209, 176, 81);
+  noStroke();
+  rect(20,70,350,250);
+  
+  fill(181, 195, 181);
+  noStroke();
+  rect(60,120,275,200);
+  
+  fill(90, 169, 160);
+  noStroke();
+  rect(80,155,40,70);
+  
+  fill(209, 176, 81);
+  noStroke();
+  rect(140,120,125,105);
+  
+  fill(81, 160, 153);
+  noStroke();
+  rect(280,155,35,70);
+  
+  rect(20,320,350,250);
+  
+  
+} 
