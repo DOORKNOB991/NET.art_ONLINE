e28a93b9f443cff6cf004c486393c5030e40ddda
DOORKNOB991 <103093455+DOORKNOB991@users.noreply.github.com>
2023-11-05 22:51:55 -0500

    Add files via upload

---
+++ animation/sketch.js
@@ -0,0 +1,35 @@
+var angle = 0.0;
+var speed = 0.05;
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+ var lo = map(mouseX, 0, width, 10, 300);
+   var li = map(mouseY, 0, width, 10, 300);
+  
+  translate(width/2,height/2);
+  rotate(angle);
+  
+  for(var i = 0; i < 20; i++)
+    {
+      push();
+      rotate(i+TWO_PI/2);
+      translate(0,lo);
+     
+       r = random(255); // r is a random number between 0 - 255
+      g = random(100,200); // g is a random number betwen 100 - 200
+      b = random(100); // b is a random number between 0 - 100
+      a = random(200,255); // a is a random number between 200 - 255
+      fill(r, g, b,a);
+       triangle(10, 10,320, 100, 310,
+               80);
+      pop();
+    }
+  angle +=speed;
+
+}
+
+
