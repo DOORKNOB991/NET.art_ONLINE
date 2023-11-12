f31e557e002290ab6a01274368bbe05e601d04ec
DOORKNOB991 <103093455+DOORKNOB991@users.noreply.github.com>
2023-11-12 16:26:31 -0500

    Update sketch.js

--- animation/sketch.js
+++ animation/sketch.js
@@ -5,7 +5,7 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(143, 188, 143);
   
  var lo = map(mouseX, 0, width, 10, 300);
    var li = map(mouseY, 0, width, 10, 300);
